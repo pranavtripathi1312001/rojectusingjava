9057a44d0704af3b7264bc5c7144fdcccdf03451
pranavtripathi1312001 <140252345+pranavtripathi1312001@users.noreply.github.com>
2023-07-22 17:53:01 +0530

    Add files via upload

---
+++ CharacterLiteral.java
@@ -0,0 +1,11 @@
+class IntegerLiteralQues{
+    static public void main(String s[]){
+    int a=05; 
+    int b=010; 
+//    int c=08; error 
+//    int d=09; error 
+   System.out.println(a);
+   System.out.println(b);
+  
+    }
+}
